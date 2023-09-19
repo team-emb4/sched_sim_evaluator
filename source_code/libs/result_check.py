e1e685a28e62b5c2dba93a3eedd79eaa3455c745
kobayu <yutaro.kobayashi@tier4.jp>
2023-09-19 16:21:33 +0900

    docs: deleted extra doc

--- source_code/libs/result_check.py
+++ source_code/libs/result_check.py
@@ -7,7 +7,6 @@ from libs import util
 from libs.util import algo_list
 
 
-# カスタムタグの定義
 # !Schedulableタグの定義
 class SchedulableTag:
     def __init__(self, high_dedicated_cores, low_dedicated_cores):
@@ -60,19 +59,14 @@ def read_yaml_file(file_path):
         return data
 
 
-# ディレクトリ内のファイルのうち、"result: !Schedulable"と"result: !Unschedulable"の数をカウント
+# ディレクトリ内のyamlファイル群から、スケジュール成功とスケジュール失敗の数をカウント
 def count_results(root_log_dir_path):
-    # 入力ディレクトリの絶対パスを取得
     if not os.path.isabs(root_log_dir_path):
         root_log_dir_path = os.path.abspath(root_log_dir_path)
 
-    # 入力ディレクトリパスのSchedResult以降を削除
-    algorithm_dir = root_log_dir_path.split("/SchedResult")[0]
-
-    # アルゴリズムのディレクトリ名を取得
-    algorithm = os.path.basename(algorithm_dir)
-    if algorithm not in algo_list:
-        # 使用可能なアルゴリズム名を表示
+    algo_dir = root_log_dir_path.split("/SchedResult")[0]
+    algo_name = os.path.basename(algo_dir)
+    if algo_name not in algo_list:
         util.print_log(
             "Algorithm name is not correct. Available algorithm names are:", log_kind="ERROR"
         )
@@ -80,18 +74,14 @@ def count_results(root_log_dir_path):
             print(f"  {key}")
         exit(1)
     else:
-        properties = algo_list[algorithm]  # アルゴリズムのプロパティを取得
-        # 実行モードが2種類ある場合はノンプリエンプティブとプリエンプティブのどちらであるかを取得
-        if properties["preemptive"] == "true":
+        algo_properties = algo_list[algo_name]  # アルゴリズムのプロパティを取得
+        if algo_properties["preemptive"] == "true":
             pre = os.path.basename(os.path.dirname(root_log_dir_path))
-            util.print_log("algorithm: " + algorithm + "-" + pre)
+            util.print_log("Target algorithm: " + algo_name + "-" + pre)
         else:
-            util.print_log("algorithm: " + algorithm)
+            util.print_log("Target algorithm: " + algo_name)
 
-    # 入力ディレクトリの中にあるディレクトリの数をカウント
     log_dir_count = len(os.listdir(root_log_dir_path))
-
-    # カウント結果を格納する配列を初期化
     max_utilization = [0] * log_dir_count
     yaml_count = [0] * log_dir_count
     schedulable_count = [0] * log_dir_count
@@ -101,63 +91,46 @@ def count_results(root_log_dir_path):
 
     dir_list = os.listdir(root_log_dir_path)
     dir_list.sort()
-
-    # 入力ディレクトリの中にあるディレクトリごとに処理
     for i, log_dir in enumerate(dir_list):
         util.print_log("Target directory: " + log_dir)
-        # ディレクトリ名から数値を抽出
-        number = util.extract_numbers_from_string(log_dir)
-        if number is None:
+        utilization = util.extract_numbers_from_string(log_dir)
+        if utilization is None:
             continue
-        max_utilization[i] = number
+        max_utilization[i] = utilization
 
-        # ディレクトリ内のファイルを取得
         log_dir = os.path.join(root_log_dir_path, log_dir)
         log_file_list = os.listdir(log_dir)
-
         for j, file_name in enumerate(log_file_list):
-            # プログレスバーを表示
             util.progress_bar(j, len(log_file_list))
-
-            # ファイル名が".yaml"で終わる場合のみ処理
             if file_name.endswith(".yaml"):
                 yaml_count[i] += 1
                 log_file_path = os.path.join(log_dir, file_name)
-
-                # ファイルからresultを確認
                 log_data = read_yaml_file(log_file_path)
 
-                # resultがSchedulableまたはUnschedulableで表される場合
-                if properties["result"] == "schedulability":
-                    # resultが!Schedulableの場合
+                if algo_properties["result"] == "schedulability":
                     if isinstance(log_data["result"], SchedulableTag):
                         schedulable_count[i] += 1
-                    # resultが!Unschedulableの場合
                     elif isinstance(log_data["result"], UnschedulableTag):
                         unschedulable_count[i] += 1
                     else:
                         print("Unknown Result")
 
                 else:  # resultがtrueまたはfalseで表される場合
-                    # resultがtrueの場合
                     if log_data["result"]:
                         true_count[i] += 1
-                    # resultがfalseの場合
                     else:
                         false_count[i] += 1
 
-        # プログレスバーを表示
         util.progress_bar(1, 1)
         print("")
 
-    # カウント結果を表示
     print("==============Result==============")
     accept = [0.0] * log_dir_count
     for i, subdir in enumerate(dir_list):
         print(f"Directory: {subdir}")
         print(f"  Max utilization: {max_utilization[i]}")
         print(f"  Number of .yaml files: {yaml_count[i]}")
-        if properties["result"] == "schedulability":
+        if algo_properties["result"] == "schedulability":
             print(f"  Number of schedulable: {schedulable_count[i]}")
             print(f"  Number of schedulable: {unschedulable_count[i]}")
             accept[i] = schedulable_count[i] / yaml_count[i]
@@ -169,10 +142,9 @@ def count_results(root_log_dir_path):
             print(f"  Acceptance of true: {accept[i]}")
         print("")
 
-    # 全体の結果を表示
     print("Total:")
     print(f"  Number of directories: {sum(yaml_count)}")
-    if properties["result"] == "schedulability":
+    if algo_properties["result"] == "schedulability":
         print(f"  Number of schedulable: {sum(schedulable_count)}")
         print(f"  Number of unschedulable: {sum(unschedulable_count)}")
         print(f"  Acceptance of schedulable: {sum(schedulable_count) / sum(yaml_count)}")
@@ -182,39 +154,28 @@ def count_results(root_log_dir_path):
         print(f"  Acceptance of true: {sum(true_count) / sum(yaml_count)}")
     print("")
     print("==================================")
-    # グラフを作成
-    # ディレクトリ名からコア数を取得
+
     core = os.path.basename(root_log_dir_path)
-
-    # グラフの初期化
     plt.figure()
-
     # 縦軸: Acceptance of schedulable(true)
     # 横軸: Max utilization
     plt.plot(max_utilization, accept, marker="o", color="blue", linestyle="-")
     plt.xlabel("Max utilization")
-    if properties["result"] == "schedulability":
+    if algo_properties["result"] == "schedulability":
         plt.ylabel("Acceptance of schedulable")
     else:
         plt.ylabel("Acceptance of true")
 
-    # グラフの保存先ディレクトリを作成
-    os.makedirs(f"{algorithm_dir}/OutputsResult", exist_ok=True)
-
-    # グラフを保存
-    if properties["preemptive"] == "true":
+    os.makedirs(f"{algo_dir}/OutputsResult", exist_ok=True)
+    if algo_properties["preemptive"] == "true":
         plt.savefig(
-            f"{algorithm_dir}/OutputsResult/plot_accept_{algorithm}_{pre}_{core}-cores.png"
+            f"{algo_dir}/OutputsResult/plot_accept_{algo_name}_{pre}_{core}-cores.png"
         )
     else:
-        plt.savefig(f"{algorithm_dir}/OutputsResult/plot_accept_{algorithm}_{core}-cores.png")
-
-    # グラフを表示
-    plt.show()
+        plt.savefig(f"{algo_dir}/OutputsResult/plot_accept_{algo_name}_{core}-cores.png")
 
 
 if __name__ == "__main__":
-    # コマンドライン引数からディレクトリのパスを取得
     if len(sys.argv) != 2:
         print("Usage: python result_check.py /path/to/your/directory")
         sys.exit(1)
